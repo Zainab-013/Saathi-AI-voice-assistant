def410d5f7780325a91c382474669b84987165e9
Zainab-013 <zainab.shaikh132005@gmail.com>
2026-06-11 15:12:41 +0530

    Fix langchain text splitter import and add dependency

--- ingest_data.py
+++ ingest_data.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.document_loaders.pdf import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
